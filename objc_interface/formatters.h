ed15d09ee67bd23d6a399f9e209f9487dc7a37bf
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-03-17 00:54:32 +0100

    Adds formatting of dates with a part shadowed. Refs #9.

--- objc_interface/formatters.h
+++ objc_interface/formatters.h
@@ -2,3 +2,6 @@
 
 NSString *format_date(TWeight *weight);
 NSString *format_nsdate(NSDate *date);
+
+NSAttributedString *format_shadowed_date(TWeight *weight,
+    UIFont *font, UIColor *normal, UIColor *shadowed);
--- objc_interface/formatters.m
+++ objc_interface/formatters.m
@@ -31,3 +31,46 @@ NSString *format_nsdate(NSDate *date)
     if (!formatter) return @"";
     return [formatter stringFromDate:date];
 }
+
+/** Like format_nsdate but shadows the part of the date day.
+ *
+ * This is used for the cases where a same day entry is displayed. The second
+ * (and further) entries of the same day are displayed in a shadowed color.
+ */
+NSAttributedString *format_shadowed_date(TWeight *weight,
+    UIFont *font, UIColor *normal, UIColor *shadowed)
+{
+    assert(weight && @"Bad param");
+    NSDate *d = [NSDate dateWithTimeIntervalSince1970:date(weight)];
+
+    // First create the attribute string with the normal text.
+    NSString *normal_text = format_nsdate(d);
+    NSMutableAttributedString *text = [[NSMutableAttributedString alloc]
+        initWithString:normal_text attributes:@{NSFontAttributeName:font}];
+
+    // Now find the range for the time part we want to NOT shadow.
+    static NSDateFormatter *formatter = nil;
+    if (!formatter) {
+        formatter = [NSDateFormatter new];
+        [formatter setTimeStyle:NSDateFormatterMediumStyle];
+        [formatter setDateStyle:NSDateFormatterNoStyle];
+        formatter.doesRelativeDateFormatting = YES;
+    }
+
+    // Find the range of the text we will keep in normal color.
+    NSString *part = [formatter stringFromDate:d];
+    const NSRange r = [normal_text
+        rangeOfString:part options:NSCaseInsensitiveSearch];
+    if (NSNotFound == r.location) {
+        DLOG(@"Weird, could not find '%@' in original date string?", part);
+        return text;
+    }
+
+    // By default make all the text shadowed.
+    [text addAttribute:NSForegroundColorAttributeName
+        value:shadowed range:(NSRange){0, text.length}];
+    // Then modify the part we want to be seen.
+    [text addAttribute:NSForegroundColorAttributeName
+        value:normal range:r];
+    return text;
+}
